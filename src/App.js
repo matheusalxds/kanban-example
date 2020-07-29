17522048995814095bad46cc8b7602cc22f4a1b7
Matheus Alexandre <mathe.system@gmail.com>
2020-07-29 10:34:21 -0300

    refactor: move DnD data for its container

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-import React, { useState } from 'react';
-import produce from 'immer';
+import React from 'react';
 
-import loadData from './components/DragAndDrop/__mock__/__fixture';
-
-// context
-import { Context as BoardContext } from './contexts/Context';
-
-// components
 import Main from './components/Main/Main';
 import Content from './components/Content/Content';
 import DnD from './components/DragAndDrop/DnD';
 
-// utils
-import parseData from './components/DragAndDrop/utils/parse-data';
-
-const data = loadData();
-
-function App() {
-  const [lists, setLists] = useState(parseData(data));
-
-  const move = (fromList, toList, from, to) => {
-    const resp = produce(lists, draft => {
-      const dragged = draft[fromList][from];
-      draft[fromList].splice(from, 1);
-      draft[toList].splice(to, 0, dragged);
-    });
-    setLists(prevState => {
-      return { ...prevState, ...resp };
-    });
-  };
-
-  return (
-    <div className="App">
-      <Main>
-        <Content>
-          <BoardContext data={{ lists, move }}>
-            <DnD />
-          </BoardContext>
-        </Content>
-      </Main>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Main>
+      <Content>
+        <DnD />
+      </Content>
+    </Main>
+  </div>
+)
 
 export default App;
--- src/components/DragAndDrop/DnD.js
+++ src/components/DragAndDrop/DnD.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from 'immer';
 
 // context
-import { useContextHook } from '../../contexts/Context';
+import { Context as BoardContext } from '../../contexts/Context';
 
 // components
-import DnDHeader from './DnDHeader/DnDHeader';
-import DnDContent from './DnDContent/DnDContent';
 import Col from './Col/Col';
 import Card from './Card/Card';
+import DnDHeader from './DnDHeader/DnDHeader';
+import DnDContent from './DnDContent/DnDContent';
 import DnDProvider from './DnDProvider/DnDProvider';
 
+// utils
+import parseData from './utils/parse-data';
+import loadData from './__mock__/__fixture';
+
+// fixtures
+const data = loadData();
+
 const DnD = () => {
-  const { lists } = useContextHook();
+  const [lists, setLists] = useState(parseData(data));
+
+  const move = (fromList, toList, from, to) => {
+    const resp = produce(lists, draft => {
+      const dragged = draft[fromList][from];
+      draft[fromList].splice(from, 1);
+      draft[toList].splice(to, 0, dragged);
+    });
+    setLists(prevState => {
+      return { ...prevState, ...resp };
+    });
+  };
+
   return (
+    <BoardContext data={{ lists, move }}>
     <DnDProvider>
       <DnDHeader />
       <DnDContent>
@@ -32,6 +53,7 @@ const DnD = () => {
         })}
       </DnDContent>
     </DnDProvider>
+    </BoardContext>
   );
 };
 
